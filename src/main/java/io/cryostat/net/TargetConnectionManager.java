bbdc70496b4a3df47dc02d17b208f31647181a40
Andrew Azores <aazores@redhat.com>
2023-05-30 10:29:42 -0400

    perform async task locking within worker scope, not caller scope

--- src/main/java/io/cryostat/net/TargetConnectionManager.java
+++ src/main/java/io/cryostat/net/TargetConnectionManager.java
@@ -120,22 +120,23 @@ public class TargetConnectionManager {
 
     public <T> CompletableFuture<T> executeConnectedTaskAsync(
             ConnectionDescriptor connectionDescriptor, ConnectedTask<T> task) {
-        synchronized (
-                targetLocks.computeIfAbsent(
-                        connectionDescriptor.getTargetId(), k -> new Object())) {
-            return connections
-                    .get(connectionDescriptor)
-                    .thenApplyAsync(
-                            conn -> {
-                                try {
+        return connections
+                .get(connectionDescriptor)
+                .thenApplyAsync(
+                        conn -> {
+                            try {
+                                synchronized (
+                                        targetLocks.computeIfAbsent(
+                                                connectionDescriptor.getTargetId(),
+                                                k -> new Object())) {
                                     return task.execute(conn);
-                                } catch (Exception e) {
-                                    logger.error(e);
-                                    throw new CompletionException(e);
                                 }
-                            },
-                            executor);
-        }
+                            } catch (Exception e) {
+                                logger.error(e);
+                                throw new CompletionException(e);
+                            }
+                        },
+                        executor);
     }
 
     public <T> T executeConnectedTask(
