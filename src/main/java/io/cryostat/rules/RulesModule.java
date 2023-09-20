730fee776e263a5ab309c870108d99b7ed620b22
Andrew Azores <aazores@redhat.com>
2023-09-20 11:38:30 -0400

    fixup! remove unused code

--- src/main/java/io/cryostat/rules/RulesModule.java
+++ src/main/java/io/cryostat/rules/RulesModule.java
@@ -19,7 +19,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.Executors;
-import java.util.function.Function;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
@@ -29,7 +28,6 @@ import javax.script.ScriptEngine;
 import io.cryostat.configuration.ConfigurationModule;
 import io.cryostat.configuration.CredentialsManager;
 import io.cryostat.core.log.Logger;
-import io.cryostat.core.net.Credentials;
 import io.cryostat.core.sys.FileSystem;
 import io.cryostat.discovery.DiscoveryStorage;
 import io.cryostat.net.TargetConnectionManager;
@@ -42,13 +40,11 @@ import com.google.gson.Gson;
 import dagger.Lazy;
 import dagger.Module;
 import dagger.Provides;
-import io.vertx.core.MultiMap;
 
 @Module
 public abstract class RulesModule {
     public static final String RULES_SUBDIRECTORY = "rules";
     public static final String RULES_WEB_CLIENT = "RULES_WEB_CLIENT";
-    public static final String RULES_HEADERS_FACTORY = "RULES_HEADERS_FACTORY";
 
     @Provides
     @Singleton
@@ -133,9 +129,7 @@ public abstract class RulesModule {
 
     @Provides
     @Singleton
-    static PeriodicArchiverFactory providePeriodicArchivedFactory(
-            @Named(RULES_HEADERS_FACTORY) Function<Credentials, MultiMap> headersFactory,
-            Logger logger) {
+    static PeriodicArchiverFactory providePeriodicArchivedFactory(Logger logger) {
         return new PeriodicArchiverFactory(logger);
     }
 }
